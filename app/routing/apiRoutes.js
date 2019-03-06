43db7b395eceb1a7bb179c37246a607f32fbc9a2
bullsean <41093836+bullsean@users.noreply.github.com>
2019-03-05 21:17:33 -0600

    Routing files done

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,16 @@
+
+//    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+var friendsData = require("../data/friends.js");
+
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
+
+    app.post("/api/friends", function(req, res) {
+        friendsData.push(req.body);
+        res.json(true);
+    });
+};
