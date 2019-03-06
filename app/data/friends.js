16784072a267de312c0f9db2aacf5014bd75573e
bullsean <41093836+bullsean@users.noreply.github.com>
2019-03-05 21:45:27 -0600

    Copied in the survey html

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friendsArray = [
+    {
+      name: "Ahmed",
+      photoLink: "example.jpeg",
+      q1: 1,
+      q2: 2,
+      q3: 3,
+      q4: 4,
+      q5: 5,
+      q6: 3,
+      q7: 3,
+      q8: 4,
+      q9: 4,
+      q10: 5
+    }
+  ];
+  
+  module.exports = friendsArray;
